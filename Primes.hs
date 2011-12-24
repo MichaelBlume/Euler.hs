6c1598413b3bb2e0baea66c2c614600ec0aab416
Mike Blume <mike@loggly.com>
2011-12-24 11:04:06 -0800

    split into modules

---
+++ Primes.hs
@@ -0,0 +1,23 @@
+module Primes
+( primes
+) where
+
+import Helpers
+
+isPrime :: (Integral a) => a -> Bool
+isPrime 2 = True
+isPrime x = not $ any (divs x) $ primesBelowSqrt x
+
+primesBelowSqrt :: (Integral a) => a -> [a]
+primesBelowSqrt square = pbHelp primeMap where
+    pbHelp ((num, prime):rst)
+        | num * num > square = []
+        | not prime = pbHelp rst
+        | otherwise = num:(pbHelp rst)
+
+primeMap :: (Integral a) => [(a, Bool)]
+primeMap = map (\x -> (x, isPrime x)) [2..]
+
+primes :: (Integral a) => [a]
+primes = map first $ filter second $ primeMap
+
