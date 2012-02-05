9b6c17c7886917ab7efd7e47a7063a6428bc9d38
Mike Blume <mike@loggly.com>
2012-02-05 11:09:42 -0800

    memoize this more sanely

--- Helpers.hs
+++ Helpers.hs
@@ -27,8 +27,7 @@ fibs :: (Integral n) => [n]
 fibs = 1:1: (zipWith (+) (tail fibs) fibs)
 
 
-sumProDivs = (myData !!) where
-  myData = map helper [0..]
+sumProDivs = memoizeF helper where
   helper 0 = 0
   helper n = (sum $ allDivs n) - n
 
@@ -66,3 +65,24 @@ encodeDirect = foldr stepper [] where
   stepper x ps@((n,el):rp)
     | x == el = (n+1,x):rp
     | otherwise = (1,x):ps
+
+data InfiniteTree a = ITree a (InfiniteTree a) (InfiniteTree a)
+
+iNums :: (Integral n) => InfiniteTree n
+iNums = construct 0 1 where
+  construct start step = ITree start left right where
+    left = construct (start + step) (step * 2)
+    right = construct (start + step * 2) (step * 2)
+
+instance Functor InfiniteTree where
+  fmap f (ITree a left right) = ITree (f a) (fmap f left) (fmap f right)
+
+seek :: (Integral i) => InfiniteTree a -> i -> a
+seek (ITree a _ _) 0 = a
+seek (ITree _ l r) n
+  | divs n 2 = seek r $ div n 2 - 1
+  | otherwise = seek l $ div n 2
+
+memoizeF :: (Integral n) => (n -> a) -> n -> a
+memoizeF f = seek myData where
+  myData = fmap f iNums
